d30ded5fb175cf4b2e1d6c03983ad272f8da906c
ElenaHrudavenka <kisseli@mail.ru>
2022-05-25 12:00:18 +0300

    Create changePasswordReducer.ts

---
+++ src/redux/changePasswordReducer.ts
@@ -0,0 +1,9 @@
+const changePasswordReducer = (state: any, action: any) => {
+    switch (action.type) {
+        /*        case '':
+                    return {...state}*/
+        default:
+            return state
+    }
+}
+export default changePasswordReducer
