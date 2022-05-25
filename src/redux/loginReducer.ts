7e8e17b5becb87391079079d1277d4aa36bf9230
ElenaHrudavenka <kisseli@mail.ru>
2022-05-25 12:00:14 +0300

    Create loginReducer.ts

---
+++ src/redux/loginReducer.ts
@@ -0,0 +1,11 @@
+
+const loginReducer = (state: any, action: any) => {
+    switch (action.type) {
+/*        case '':
+            return {...state}*/
+        default:
+            return state
+    }
+}
+
+export default loginReducer
